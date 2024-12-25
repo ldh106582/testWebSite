5936c938630a85e0d3192c255614d3ba59ddb821
unknown <ldh106582@naver.com>
2024-12-25 16:49:48 +0900

    src/view/examTypeStatus delete 기능 추가

--- src/stores/useExamTypeStore.js
+++ src/stores/useExamTypeStore.js
@@ -1,17 +1,21 @@
 import { defineStore } from "pinia";
 import axios from "@/axios";
-import { ref } from "vue";
+import { ref,onMounted  } from "vue";
 
 export const useExamTypeStore = defineStore('examTypes', () => {
 
     const list = ref([]);
 
     async function init() {
-        await axios.get('/examTypes')
-        .then (res => { 
-            list.value = res.data.rows
-        });
+        try {
+            const res = await axios.get('/examTypes');
+            list.value = res.data.rows;
+        } catch (error) {
+            console.error("Error fetching exam types:", error);
+        }
     }
+
+    onMounted(init); // 컴포넌트가 마운트될 때 init 호출
 
     return { list, init };
 });
--- src/views/ExamTypeUpdateView.vue
+++ src/views/ExamTypeUpdateView.vue
@@ -22,12 +22,12 @@
                 </v-autocomplete>
             </v-col>
             <v-col cols="12" class="py-0 px-3 d-flex justify-end">
-                <v-btn color="indigo" >유형조회</v-btn>
+                <v-btn color="indigo" @click="search">유형조회</v-btn>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="12" class="pb-5">
-                <span class="examType-search">데이터를 수정 및 삭제</span>
+                <span class="examType-search">데이터 수정 및 삭제</span>
             </v-col>
         </v-row>
 
@@ -38,7 +38,17 @@
         </v-row>
 
         <v-row class="px-3" v-else>
-            <v-col cols="12" class="examType-search" >
+            <v-col class="examType-typeName px-1" cols="11" >
+                <v-text-field variant="outlined" hide-details v-model="examStorages.type_name" />
+            </v-col>
+            <v-col cols="1" class="d-flex align-center px-0 justify-end">
+                <v-btn color="red" @click="examTypeDelete(examStorages.type_id)">삭제</v-btn>
+            </v-col>
+            <v-col cols="12" class="examType-description px-1" >
+                <v-textarea variant="outlined" hide-details class="examType-description-text" v-model="examStorages.description" />
+            </v-col>
+            <v-col class="px-1 pt-1 d-flex justify-end" >
+                <!-- <v-btn color="primary" @click="examTypeSave(examStorage.type_id)">저장</v-btn> -->
             </v-col>
         </v-row>
     </v-container>
@@ -46,20 +56,62 @@
 
 <script setup>
 import axios from '@/axios';
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import { useExamTypeStore } from '@/stores/useExamTypeStore';
 
 const isCheckData = ref(true);
+const changeExamStorage = ref({});
 
 const examTypeStore = useExamTypeStore();
 
-function test() {
-    console.log(examTypeStore)
+function search () {
+    const errorMsg = '알 수 없는 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.';
+    const typeNameNull = '데이터를 먼저 입력해주세요.';
+
+    if (examTypeStore.type_name === undefined) {return alert (typeNameNull)}
+
+    axios.get('/search-examType', {
+        params: {
+            examSubject: examTypeStore.type_name
+        }
+    }).then(res => {
+        const data = res.data;
+
+        if (data.result) {
+            alert (errorMsg)
+        } else {
+            examStorages.value = data.rows[0];
+
+            isCheckData.value = false;
+        }
+    });
 }
 
-onMounted(() => {
-    test();
-});
+function examTypeDelete (id) {
+    const confirmMsg = '되돌릴 수 없습니다. 정말 삭제하시겠습니까?';
+    const cancelMsg = '취소되었습니다.';
+    const errorMsg ='진행 중 오류를 발견하였습니다.';
+    const succesMsg = '삭제되었습니다.';
+
+    if (!confirm (confirmMsg)) { return alert (cancelMsg) };
+
+    axios.delete('/delete-examType', {
+        params: {
+            type_id: id,
+        }
+    }).then(res => {
+        const data = res.data;
+
+        data.result === true ? alert(errorMsg) : alert (succesMsg);
+
+    });
+
+}
+
+function examTypeSave (id) {
+
+}
+
 </script>
 
 <style lang="css" scoped>
@@ -73,4 +125,9 @@ onMounted(() => {
     border: 1px solid silver; 
     border-radius: 8px;
 }
+
+.examType-description{
+    width: 100%;
+}
+
 </style>
