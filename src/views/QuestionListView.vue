e2cc3b03b1a67bf309afabe73482403441069d66
unknown <ldh106582@naver.com>
2025-01-19 10:19:22 +0900

    checkbox All 선택

--- src/views/QuestionListView.vue
+++ src/views/QuestionListView.vue
@@ -46,14 +46,22 @@
                 <v-autocomplete :items="examTypeStore.list" item-title="exam_name" item-value="exam_id" v-model="examTypeStore.exam_id" />
             </v-col>
             <v-col cols="2">
-                <v-select v-model="selectedType" :items="questionType" multiple>
+                <v-select variant="outlined" v-model="selectedType" :items="questionType" multiple>
+                    <template v-slot:prepend-item>
+                        <v-list-item title="All" @click="selectedAll">
+                            <template v-slot:prepend>
+                            <v-checkbox-btn v-model="isCheckAll"></v-checkbox-btn>
+                            </template>
+                        </v-list-item>
+                        <v-divider class="mt-2"></v-divider>
+                    </template>
                     <template v-slot:selection="{ item, index }">
                         <v-chip v-if="index < 1">
-                            <span>{{ item.title }}</span>
+                            <span>{{ isCheckAll === true ? "All" : item.title }}</span>
                         </v-chip>
-                        <span v-if="index === 1" class="text-grey text-caption align-self-center">
-                            (+{{ value.length - 1 }} others)
-                        </span>
+                    <span v-if="index === 1" class="text-grey text-caption align-self-center">
+                        (+{{ selectedType.length - 1 }} others)
+                    </span>
                     </template>
                 </v-select>
             </v-col>
@@ -74,13 +82,19 @@
 import { ref } from 'vue';
 import { useExamTypeStore } from '@/stores/useExamTypeStore';
 
-
 const examTypeStore = useExamTypeStore();
 
 const questionType = ref(['주관식', '객관식', '서술형', '단답형']);
-const selectedType = ref('All');
+const selectedType = ref([]);
+const isCheckAll = ref(false);
 const questionLevel = ref(['쉬움', '보통', '어려움']);
 const selectedLevel = ref('All')
+
+function selectedAll () {
+    isCheckAll.value = !isCheckAll.value;
+    isCheckAll.value !== true ? selectedType.value = [] : selectedType.value = questionType.value.slice();
+}
+
 </script>
 
 <style scoped>
