5fd5110b5bbba78d274b51ac206d1933e4061da5
unknown <ldh106582@naver.com>
2025-01-23 06:45:09 +0900

    views 수정중

--- src/views/QuestionListView.vue
+++ src/views/QuestionListView.vue
@@ -15,10 +15,10 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-col cols="2">
+            <v-col cols="2" class="py-0 pl-2">
                 <v-text-field hide-details variant="outlined" type="date" />
             </v-col>
-            <v-col cols="2">
+            <v-col cols="2" class="py-0 pl-2">
                 <v-text-field hide-details variant="outlined" type="date" max="9999-12-31"/>
             </v-col>
         </v-row>
@@ -125,36 +125,49 @@
                     </template>
                 </v-select>
             </v-col>
+            <v-col class="search">
+                <v-btn color="primary">검색</v-btn>
+            </v-col>
         </v-row>
 
         <v-row>
-            <v-col>
+            <v-col cols="12">
                 <div>
                     <p class="question-title">시험 문제 리스트</p>
                 </div>
                 <v-table>
                     <thead>
-                        <tr class="question-title-header">
-                            <td>No</td>
-                            <td>시험유형</td>
-                            <td>난이도</td>
-                            <td>출제년도</td>
-                            <td>출제회차</td>
-                            <td>시험문제</td>
+                        <tr class="question-header">
+                            <td class="question-content">Date</td>
+                            <td class="question-content">시험유형</td>
+                            <td class="question-content">난이도</td>
+                            <td class="question-content">출제년도</td>
+                            <td class="question-content">출제회차</td>
+                            <td class="question-question">시험문제</td>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="(rows, index) in questions" :key="index">
-                            <td>{{ index + 1 }}</td>
-                            <td> {{ getFullDate(rows.create_date) }}</td>
-                            <td>{{ rows.question_type}}</td>
-                            <td> {{ rows.question_level }}</td>
-                            <td>{{ rows.question_year }}</td>
-                            <td>{{ rows.question_academic_year }}</td>
-                            <td> {{  rows.question }}</td>
+                        <tr v-for="(rows, index) in questions" :key="index" id="quesions" @click="showQuestion">
+                            <td class="question-content"> {{ getFullDate(rows.create_date) }}</td>
+                            <td class="question-content">{{ rows.question_type}}</td>
+                            <td class="question-content"> {{ rows.question_level }}</td>
+                            <td class="question-content">{{ rows.question_year }}</td>
+                            <td class="question-content">{{ rows.question_academic_year }}</td>
+                            <td class="question-question">{{ rows.question }}</td>
                         </tr>
                     </tbody>
                 </v-table>
+            </v-col>
+        </v-row>
+        <v-row  justify="center">
+            <v-col cols="1" class="pagenation">
+                <v-btn class="pagenation-btn">이전</v-btn>
+            </v-col>
+            <v-col cols="2" class="pagenation"> 
+                <div> 1 / 30 </div>
+            </v-col>
+            <v-col cols="1" class="pagenation">
+                <v-btn class="pagenation-btn">다음</v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -180,25 +193,28 @@ const isYearCheckAll = ref(false);
 const selectedAcademicYears = ref([]);
 const isAchademicCheckAll = ref(false);
 const questions = ref([]);
+const page = ref(1);
+const countPage = ref(30);
 
 function selectedTypeAll () {
     isTypeCheckAll.value = !isTypeCheckAll.value;
     isTypeCheckAll.value !== true ? selectedTypes.value = [] : selectedTypes.value = questionTypes.value.slice();
 }
-
 function selectedLevelAll () {
     isLevelCheckAll.value = !isLevelCheckAll.value;
     isLevelCheckAll.value !== true ? selectedLevels.value = [] : selectedLevels.value = questionLevels.value.slice();
 }
-
 function selectedYearAll () {
     isYearCheckAll.value = !isYearCheckAll.value;
     isYearCheckAll.value !== true ? selectedYears.value = [] : selectedYears.value = questionYears.value.slice();
 }
-
 function selectedAcademicYearAll () {
     isAchademicCheckAll.value = !isAchademicCheckAll.value;
     isAchademicCheckAll.value !== true ? selectedAcademicYears.value = [] : selectedAcademicYears.value = questionAcademicYears.value.slice();
+}
+
+function showQuestion () {
+
 }
 
 function search () {
@@ -208,15 +224,16 @@ function search () {
             question_type: selectedTypes.value,
             question_year: selectedYears.value,
             question_academic_year: selectedAcademicYears.value,
-            question_level: selectedLevels.value
+            question_level: selectedLevels.value,
+            page: ( page.value - 1),
+            countPage: ( countPage.value - 1),
         }
     }).then(res => {
-        // res.data.rows.forEach(q => {
-        //     q.create_date = getFullDate(q.create_date);
-        // });
+        res.data.rows.forEach(q => {
+            q.create_date = getFullDate(q.create_date);
+        });
 
-        // questions.value.push(res.data.rows);
-
+        questions.value = res.data.rows;
     });
 }
 
@@ -233,7 +250,7 @@ onMounted(() => {
     font-weight: bold;
 }
 
-.question-title-header{
+.question-header{
     font-size: 15px;
     font-weight: bold;
 }
@@ -242,4 +259,40 @@ td{
     border-bottom: 1px solid black;
 }
 
+.question-content{
+    width: 10%;
+}
+
+.question-question{
+    width: 50%;
+}
+
+#quesions:hover {
+    cursor: pointer;
+}
+
+.search {
+    align-content: center;
+    text-align: end;
+}
+
+.pagenation {
+    text-align: center;
+}
+
+.pagenation-btn:hover {
+    background: silver;
+    animation-name: pagenation-btn;
+    background-size: 0% 100%; /* 초기 배경 크기 */
+    animation-duration: 1s; /* 애니메이션 지속 시간 */
+}
+
+@keyframes pagenation-btn {
+    0% {
+        background-color: transparent; /* 시작은 투명 */
+    }
+    100% {
+        background-color: silver; /* 끝은 은색 */
+    }
+}
 </style>
