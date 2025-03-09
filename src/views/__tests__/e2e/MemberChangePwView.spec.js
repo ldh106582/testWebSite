f2370b291204602c0fb1e93ab46275bec1a9a820
unknown <ldh106582@naver.com>
2025-03-09 18:47:57 +0900

    비밀번호 찾기 페이지 테스트 코드

---
+++ src/views/__tests__/e2e/MemberChangePwView.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+const url = 'http://localhost:5173/member-changepw';
+
+test.beforeEach(async ({ page }) => {
+
+});
+
+test.describe('', async () => {
+    test('', async ({ page }) => {
+
+    });
+});
