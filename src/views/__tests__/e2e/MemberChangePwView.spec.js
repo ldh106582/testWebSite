6a8155cb198be0a5bdb853ac452071c229a3aa48
unknown <ldh106582@naver.com>
2025-03-10 07:01:15 +0900

    비밀번호 찾기 페이지 테스트 코드 작성 중

--- src/views/__tests__/e2e/MemberChangePwView.spec.js
+++ src/views/__tests__/e2e/MemberChangePwView.spec.js
@@ -1,13 +1,14 @@
+import MemberChangePwView from "@/views/MemberChangePwView.vue";
 import { test, expect } from "@playwright/test";
 
 const url = 'http://localhost:5173/member-changepw';
 
 test.beforeEach(async ({ page }) => {
-
+    await page.goto(url);
 });
 
-test.describe('', async () => {
-    test('', async ({ page }) => {
+test.describe('MemberChangePwView', async () => {
+    test('아이디 입력 시 DB에 존재하지 않을 경우', async ({ page }) => {
 
     });
 });
