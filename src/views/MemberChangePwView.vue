8b7a739448282e3453192f009389155b3adedabb
unknown <ldh106582@naver.com>
2025-03-11 07:00:58 +0900

    비밀번호 변경 페이지 테스트 코드 완료

--- src/views/MemberChangePwView.vue
+++ src/views/MemberChangePwView.vue
@@ -8,14 +8,15 @@
                     </v-col>
 
                     <v-col>
-                        <v-text-field cols="12" type="text" v-model="userPw" label="임시 비밀전호" hide-details ></v-text-field>
+                        <v-text-field data-test="userPw" cols="12" hide-details type="text" v-model="userPw" label="임시 비밀전호" />
                     </v-col>
                     <v-col>
-                        <v-text-field cols="12" type="password" v-model="newPw" label="새로운 비밀전호" @keydown.enter="changePw" focused :rules="rulesPw"></v-text-field>
+                        <v-text-field data-test="newPw" cols="12" type="password" v-model="newPw" label="새로운 비밀전호" 
+                        @keydown.enter="changePw" focused :rules="rulesPw" />
                     </v-col>
 
                     <v-col class="mt-2" cols="12" lg="12" sm="12">
-                        <v-btn color="rgb(26, 32, 53)" width="100%" @click="changePw">비밀번호 변경</v-btn>
+                        <v-btn data-test="changPw" color="rgb(26, 32, 53)" width="100%" @click="changePw">비밀번호 변경</v-btn>
                     </v-col>
                 
                 </v-col>
@@ -45,8 +46,13 @@ function changePw() {
     const checkPw = '새로운 비밀번호를 입력해주세요';
     const errorMsg = '예상하지 못한 오류가 발생하였습니다. 잠시 후 다시 시도해주세요';
     const successMsg = '비밀번호가 정상적으로 변경되었습니다.';
+    const differencePw = '두 개의 비밀번호가 다릅니다. 다시 시도해주세요.';
 
-    if (newPw.value === '') return alert (checkPw);
+    if (newPw.value === '') {
+        return alert (checkPw);
+    } else if (newPw.value !== userPw) {
+        return alert (differencePw);
+    }
 
     axios.put('/member-changepw', {
         userId: router.currentRoute.value.query.userId,
--- src/views/__tests__/e2e/MemberChangePwView.spec.js
+++ src/views/__tests__/e2e/MemberChangePwView.spec.js
@@ -1,14 +1,59 @@
-import MemberChangePwView from "@/views/MemberChangePwView.vue";
 import { test, expect } from "@playwright/test";
+import { aw } from "vitest/dist/chunks/reporters.D7Jzd9GS";
 
 const url = 'http://localhost:5173/member-changepw';
+const mockUserPw = 'template123!';
+const mockNewPw = 'template123@'
 
 test.beforeEach(async ({ page }) => {
     await page.goto(url);
 });
 
 test.describe('MemberChangePwView', async () => {
-    test('아이디 입력 시 DB에 존재하지 않을 경우', async ({ page }) => {
+    test('비밀번호 변경', async ({ page }) => {
+        const msg1 = '새로운 비밀번호를 입력해주세요';
 
+        page.on('dialog', async dialog => {
+            const message = dialog.message();
+            expect(message).toBe(msg1);
+            await dialog.accept();
+        });
+
+        await page.fill('[data-test="userPw"] input', mockUserPw);
+        await page.click('[data-test="changPw"]');
     });
+
+    test('두 개의 비밀번호가 맞지 않을 때', async ({ page }) => {
+        const msg1 = '두 개의 비밀번호가 다릅니다. 다시 시도해주세요.';
+
+        page.on('dialog', async dialog => {
+            const message = dialog.message();
+            expect(message).toBe(msg1);
+            await dialog.accept();
+        });
+    
+        await page.fill('[data-test="userPw"] input', mockUserPw);
+        await page.fill('[data-test="newPw"] input', mockNewPw);
+        await page.click('[data-test="changPw"]');
+    });
+
+    test('', async ({ page }) => {
+        const msg1 = '비밀번호가 정상적으로 변경되었습니다.';
+
+        page.on('dialog', async dialog => {
+            const message = dialog.message();
+            expect(message).toBe(msg1);
+            await dialog.accept();
+        });
+
+        await page.fill('[data-test="userPw"] input', mockUserPw);
+        await page.fill('[data-test="newPw"] input', mockUserPw);
+        await page.click('[data-test="changPw"]');
+
+        Promise.all([
+            page.waitForResponse(res => res.url().includes('/member-changepw') && res.status() === 200)
+        ]);
+        
+    });
+
 });
