c3e3099997b971819841f069e4e47fa5d7ae87b1
unknown <ldh106582@naver.com>
2025-04-16 07:14:53 +0900

    웹크롤링 데이터 문제 출력까지 완성

--- src/pythonviews/getProblemScraping.py
+++ src/pythonviews/getProblemScraping.py
@@ -5,6 +5,14 @@ url = 'https://chobopark.tistory.com/196'
 response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
+num = []
+for i in range(20) :
+    num.append(f'{i+1}.')
 
-title = soup.find_all('b')
-print(title)
+problem = soup.find_all('b')
+# sliceOfProblem = proplem.find('<b>')
+# answer = soup.select('#content > div > div.entry-content > div.tt_article_useless_p_margin.contents_style > div:nth-child(27)')
+# print(proplem)
+
+question = []
+for p in problem :
