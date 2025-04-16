3319c146971e16d0244e1467d3dea7deb5459543
unknown <ldh106582@naver.com>
2025-04-17 00:08:02 +0900

    웹크롤링으로 문제 출력 완료 답 출력 해야 함

--- src/pythonviews/getProblemScraping.py
+++ src/pythonviews/getProblemScraping.py
@@ -8,11 +8,20 @@ soup = BeautifulSoup(html, 'html.parser')
 num = []
 for i in range(20) :
     num.append(f'{i+1}.')
+answerTitle = []
 
 problem = soup.find_all('b')
-# sliceOfProblem = proplem.find('<b>')
-# answer = soup.select('#content > div > div.entry-content > div.tt_article_useless_p_margin.contents_style > div:nth-child(27)')
-# print(proplem)
+# sliceOfProblem = problem.find('<b>')
+for i in range(100) :
+    selector = f'#content > div > div.entry-content > div.tt_article_useless_p_margin.contents_style > div:nth-child({i})'
+    answer = soup.select(selector)
+    
+
 
 question = []
-for p in problem :
+for p in problem:
+    text = p.get_text()
+    for n in num:
+        if n in text:
+            question.append(text)
+            break
