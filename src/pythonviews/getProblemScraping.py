a625a34f812a993c5bd26f9551b425b27816d63a
unknown <ldh106582@naver.com>
2025-04-18 07:12:00 +0900

    웹크롤링 답 문제 코드문제 추출 성공

--- src/pythonviews/getProblemScraping.py
+++ src/pythonviews/getProblemScraping.py
@@ -5,23 +5,43 @@ url = 'https://chobopark.tistory.com/196'
 response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
-num = []
-for i in range(20) :
-    num.append(f'{i+1}.')
-answerTitle = []
 
-problem = soup.find_all('b')
-# sliceOfProblem = problem.find('<b>')
-for i in range(100) :
-    selector = f'#content > div > div.entry-content > div.tt_article_useless_p_margin.contents_style > div:nth-child({i})'
-    answer = soup.select(selector)
+def getQuestion () :
+    codeQuestion = '코드이다.'
+    num = []
+    for i in range(20) :
+        num.append(f'{i+1}.')
     
+    ## 문제
+    problem = soup.find_all('b')
+    question = []
+    for p in problem:
+        text = p.get_text()
+        for n in num:
+            if n in text:
+                question.append(text)
+                if codeQuestion in text:
+                    question.append(getCodeQuestion())
+                    break
+    return question
 
+def getCodeQuestion () :
+    codeQuestion = []
+    code = soup.find_all('code')
+    for c in code:
+        return c.get_text()
 
-question = []
-for p in problem:
-    text = p.get_text()
-    for n in num:
-        if n in text:
-            question.append(text)
-            break
+def getAnswer () :
+    ## 답
+    answer_2 = []
+    for i in range(100) :
+        selector = f'#content > div > div.entry-content > div.tt_article_useless_p_margin.contents_style > div:nth-child({i})'
+        answer = soup.select(selector)
+        answer_2 = soup.select('.moreless-content > p > span')
+        print(answer_2)
+    # for a in answer_2:
+    #     text = a.get_text()
+
+getQuestion()
+getCodeQuestion()
+getAnswer()
