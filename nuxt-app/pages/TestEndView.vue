bfcaaff1c765f63176d1487ce7cc07177966cb0d
DoHyeon Lee <ldh106582@naver.com>
2025-05-23 06:54:13 +0900

    src/views/nuxt/TestEndView 디자인 구성 중중

--- nuxt-app/pages/TestEndView.vue
+++ nuxt-app/pages/TestEndView.vue
@@ -5,7 +5,26 @@
                 <v-col cols="10">
                     <span>{{ index + 1 }}번.   {{ problem.question }}</span>
                 </v-col>
+
+                <v-col cols="12" class="d-flex pa-0" >
+                    <img v-if="problem.problem_image" :src="problem.problem_image" alt="Image" class="shadow-md rounded-xl w-full sm:w-64"
+                    style="width: 100%; max-height: 500px;" />
+                </v-col>
+
+                <v-col cols="12">
+                    <v-textarea v-if="problem.problem" data-test="problem" variant="outlined" hide-details
+                    auto-grow v-model="problem.problem" readonly/>
+                </v-col>
+                
+                <v-col cols="1" style="text-align: center; align-content: center;">
+                    <span>제출한 답 :  </span>
+                </v-col>
+
+                <v-col>
+                    의견을 작성하기
+                </v-col>
             </v-row>
+            
         </v-col>
     </v-container>
 </template>
