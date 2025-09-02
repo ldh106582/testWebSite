a97466f033ed961ed5d1cda37aa096a1674f1ffd
DoHyeon Lee <ldh106582@naver.com>
2025-09-03 07:10:39 +0900

    아스키 코드 로 변환하는 테스트 코드 작성 후 본 코드 작성 완료

--- src/mixins/__tests__/useChagneASCIIAndBactick.test.js
+++ src/mixins/__tests__/useChagneASCIIAndBactick.test.js
@@ -2,6 +2,20 @@ import { describe, test, expect, vi, beforeEach } from "vitest";
 import useChangeASCIIAndBactick from '../useChangeASCIIAndBactick';
 import { flushPromises, mount } from "@vue/test-utils";
 
-describe('', () => {
-    
+describe('useChangeASCIIAndBactick', () => {
+    let wrapper = '';
+
+    beforeEach(() => {
+        wrapper = useChangeASCIIAndBactick();
+    });
+
+    test('changeASCII 함수 작동 확인', async () => {
+        const mockData = `가나다라'마바사아`;
+        const mockExpect = '가나다라[39]마바사아';
+
+        const result = await wrapper.changeASCII(mockData);
+        expect(result).toBe(mockExpect);
+    });
+
+
 });
--- src/mixins/useChangeASCIIAndBactick.js
+++ src/mixins/useChangeASCIIAndBactick.js
@@ -1,10 +1,17 @@
-export default function asciicodeANDBactick() {
+export default function useChangeASCIIAndBactick() {
 
-    function changeASCII() {
-
+    function changeASCII(param) {
+        const matchingata = /['"`]/g;
+        const transformedString = param.replace(matchingata, (match) => {
+            let charCode = match.charCodeAt(0);
+            return `[${charCode}]`;
+        });
+        return transformedString;
     }
 
     function changeBactick() {
         
     }
+
+    return {changeASCII, changeBactick}
 }
