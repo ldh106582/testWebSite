8aba7b4140f5493e59b649988848735591241dfe
DoHyeon Lee <ldh106582@naver.com>
2025-10-04 08:15:25 +0900

    잘못된 테스트 코드 삭제

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
     {
         path: '/',
         name : 'home',
-        component: () => import('@/views/home.vue'),
+        component: () => import('@/views/Home.vue'),
     },
     {
         path: '/member-create',
