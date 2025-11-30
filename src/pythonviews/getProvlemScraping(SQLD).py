b08f9f44bbfba53703e8b4a36523eaa5691a3295
DoHyeon Lee <ldh106582@naver.com>
2025-12-01 07:51:46 +0900

    SQLD 시험 문제 크롤링 준비

---
+++ src/pythonviews/getProvlemScraping(SQLD).py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://yunamom.tistory.com/'
+
+def getQuestion():
+    
+    
+    
+    return "dh"
+    
+    
+getQuestion()
