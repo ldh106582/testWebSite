49e733074432a6c17839764a4751a2b9600b9f63
DoHyeon Lee <ldh106582@naver.com>
2026-01-03 14:02:29 +0900

    오라클 클라우드 배포

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,22 +1,18 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col cols="12">
-        <v-row>
-          <v-col cols="12" sm="6">
-            <chartView class="chart" />
-          </v-col>
-          <v-col cols="12" sm="6">
-            <announcementView class="announcement" />
-          </v-col>
-        </v-row>
-        
-        <v-row style=" height: auto;">
-          <v-col cols="12" sm="12" lg="12" style="height: auto; width: 100%;">
-            <Welcome class="welcome" />
-          </v-col>
-        </v-row>
+      <v-col cols="6" sm="6">
+        <chartView class="chart" />
       </v-col>
+      <v-col cols="6" sm="6">
+        <announcementView class="announcement" />
+      </v-col>
+        
+      <v-row style=" height: auto;">
+        <v-col cols="12" sm="12" lg="12" style="height: auto; width: 100%;">
+          <Welcome class="welcome" />
+        </v-col>
+      </v-row>
     </v-row>
   </v-container>
 </template>
