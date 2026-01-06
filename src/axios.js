8d6b06b3c6ee973efee28d7b7694d977bac87518
DoHyeon Lee <ldh106582@naver.com>
2026-01-06 22:43:18 +0900

    oracle cloud private 추가

--- src/axios.js
+++ src/axios.js
@@ -1,20 +1,26 @@
 import axios from 'axios';
 
+// 현재 환경이 'production'인지 확인 (npm run build 등으로 실행 시 자동 설정됨)
+const isProduction = process.env.NODE_ENV === 'production';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: isProduction 
+    ? 'http://134.185.97.170:3000/' 
+    : 'http://localhost:3000/'
 });
 
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('Authorization')
+    const token = localStorage.getItem('Authorization');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-      config.headers["Content-Type"] = "application/json"
-    } else {
-       // Do something... Usually logout user.
+      config.headers.Authorization = `Bearer ${token}`;
+      config.headers["Content-Type"] = "application/json";
     }
-    return config
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-)
+);
 
-  export default instance
+export default instance;
