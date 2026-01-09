85501b7e134f921b077bb85492ac20315a0e5e6a
DoHyeon Lee <ldh106582@naver.com>
2026-01-09 19:42:23 +0900

    axios url 변경

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const instance = axios.create({
   baseURL: isProduction 
-    ? 'http://134.185.97.170:3000/' 
+    ? 'http://144.24.83.203:3000/' 
     : 'http://localhost:3000/'
 });
 
