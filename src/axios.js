2c28c934a220ae44bad362cff22379ad7a1a090d
DoHyeon Lee <ldh106582@naver.com>
2026-01-10 11:15:31 +0900

    axios 수정

--- src/axios.js
+++ src/axios.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 // 현재 환경이 'production'인지 확인 (npm run build 등으로 실행 시 자동 설정됨)
 const isProduction = process.env.NODE_ENV === 'production';
 
+// const instance = axios.create({
+//   baseURL: isProduction 
+//     ? 'http://examsite.duckdns.org'
+//     : 'http://localhost:3000/'
+// });
 const instance = axios.create({
-  baseURL: isProduction 
-    ? 'https://examsite.duckdns.org' 
-    : 'http://localhost:3000/'
+  baseURL: 'https://examsite.duckdns.org' // 일단 무조건 오라클 서버로 가게 수정
 });
 
 instance.interceptors.request.use(
